e18bd8b15c1153e89bce32f9dc220000ccde5fe7
Ian Bruyere <6414971+ianbruyere@users.noreply.github.com>
2020-04-02 15:33:24 -0400

    fixing some...hrmmm...mistakes

--- main.py
+++ main.py
@@ -17,31 +17,31 @@ def get_average_sentence_length(sentences):
         
     return total/len(sentences)
 
-def highlight_short_sentence(avg_length, sentence, deviation=3):
-    
+def highlight_short_sentence(sentence, deviation=3):
+    return f'**{sentence}**'
 
-def highlight_long_sentence(avg_length, sentence, deviation=3):
-    pass
+def highlight_long_sentence(sentence, deviation=3):
+    return f'*{sentence}*'
 
-def main(file_path, output_path):
+def main(file_path, output_path='output'):
     sentences = get_sentences(file_path)
     avg_length = get_average_sentence_length(sentences)
     processed = []
 
     for sentence in sentences:
         words = re.findall(r'\w+', sentence)
+        highlighted = ''
         if len(words) < avg_length:
-            highlight_short_sentences(avg_length, sentence)
-        else if len(words) > avg_length:
-            highlight_long_sentence(avg_length, sentence)
+           highlighted = highlight_short_sentence(sentence)
+        elif len(words) > avg_length:
+            highlighted = highlight_long_sentence(sentence)
         # sentence is of average length
         else:
-            pass
-        processed.push(sentence)
+            highlighted = sentence
+        processed.append(highlighted)
     
     with open(f'{output_path}.md', 'w') as f:
-        f.write(''.join(processed))
-
+        f.write('\n'.join(processed))
 
 
     
